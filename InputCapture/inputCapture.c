1ef9748b8e8e41a196ce407cd8e5075d2ae42ecb
damien <monni.damien@gmail.com>
2014-01-07 16:31:12 +0100

    Create inputCapture starting file and folder

---
+++ InputCapture/inputCapture.c
@@ -0,0 +1,16 @@
+/**************************************
+Damien MONNI - 07/01/2014
+www.damien-monni.fr
+
+Input capture first test
+**************************************/
+
+#include <avr/io.h> 
+#include <avr/interrupt.h>
+
+int main(void){
+
+	while(1);
+	
+	return 0;
+}
