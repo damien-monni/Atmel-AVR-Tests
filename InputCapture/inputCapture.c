e9dde8921401dba7a81cda52f538008ce71898e7
damien <monni.damien@gmail.com>
2014-01-07 17:14:05 +0100

    Write a first program for interrupts

--- InputCapture/inputCapture.c
+++ InputCapture/inputCapture.c
@@ -2,7 +2,7 @@
 Damien MONNI - 07/01/2014
 www.damien-monni.fr
 
-Input capture first test
+External Interrupt test on pin 43 (PD0, INT0) - Arduino pin 21
 **************************************/
 
 #include <avr/io.h> 
@@ -10,7 +10,18 @@ Input capture first test
 
 int main(void){
 
+	EICRA |= 1<<ISC00 //Any logical change on INT0 generates an interrupt request
+	EIMSK |= 1<<INT0 //Enable interrupt INT0
+	
+	sei();
+	
+	DDRB |= 1<<DDB7;
+	
 	while(1);
 	
 	return 0;
+}
+
+ISR(INT0_vect){
+	PORTB ^= 1<<PORTB7;
 }
